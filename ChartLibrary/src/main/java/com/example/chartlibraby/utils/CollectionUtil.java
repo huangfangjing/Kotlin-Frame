825eed53593d0c27441336f6a5ca1caf571c75bc
huangfangjing <3429812342@qq.com>
2024-06-30 13:24:59 +0800

    2024-6-28 show gif

--- ChartLibrary/src/main/java/com/example/chartlibraby/utils/CollectionUtil.java
+++ ChartLibrary/src/main/java/com/example/chartlibraby/utils/CollectionUtil.java
@@ -1,12 +1,6 @@
 package com.example.chartlibraby.utils;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -34,25 +28,4 @@ public class CollectionUtil {
 
         return false;
     }
-
-    /**
-     * 深度拷贝List，防止地址指向同一份
-     * @param src
-     * @param <T>
-     * @return
-     * @throws IOException
-     * @throws ClassNotFoundException
-     */
-    public static <T> List<T> deepCopy(List<T> src) throws IOException, ClassNotFoundException {
-        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-        ObjectOutputStream out = new ObjectOutputStream(byteOut);
-        out.writeObject(src);
-
-        ByteArrayInputStream byteIn = new ByteArrayInputStream(byteOut.toByteArray());
-        ObjectInputStream in = new ObjectInputStream(byteIn);
-        @SuppressWarnings("unchecked")
-        List<T> dest = (List<T>) in.readObject();
-        return dest;
-    }
-
 }
--- app/src/main/java/com/example/kotlinframe/ui/project/ProjectFragment.kt
+++ app/src/main/java/com/example/kotlinframe/ui/project/ProjectFragment.kt
@@ -15,9 +15,39 @@ class ProjectFragment : RefreshVMFragment<ProjectViewModel, FragmentProjectBindi
 
     override fun loadData() {
         viewModel.getBarData()
+        viewModel.getLineData()
+        viewModel.getRadarData()
+        viewModel.getHorData()
     }
 
     override fun initObserve() {
+        super.initObserve()
+        flowLaunch {
+            viewModel.mBarData.asSharedFlow().flowWithLifecycle(lifecycle).collect {
+                with(mBinding.barView) {
+                    mBinding.barView.buildDate(
+                        it, null, context.getString(R.string.person_sum),
+                        context.getString(R.string.score)
+                    )
+                    hideIndicate()
+                }
+            }
+        }
+        flowLaunch {
+            viewModel.mLineData.asSharedFlow().flowWithLifecycle(lifecycle).collect {
+                mBinding.lineView.buildDate(it) { twoLevelData ->
+                    showToast(twoLevelData.indexName)
+                }
+            }
+        }
+
+        flowLaunch {
+            viewModel.mRadarData.asSharedFlow().flowWithLifecycle(lifecycle).collect {
+                mBinding.aiRadarView.buildData(it) { content, _ ->
+                    showToast(content)
+                }
+            }
+        }
         flowLaunch {
             viewModel.mHorData.asSharedFlow().flowWithLifecycle(lifecycle).collect {
                 mBinding.horView.buildDate(it) { twoLevelData ->
@@ -26,4 +56,5 @@ class ProjectFragment : RefreshVMFragment<ProjectViewModel, FragmentProjectBindi
             }
         }
     }
+
 }
